64911fc0dc6ed572e42f7e14d17e38c220c6d15d
abhi291096 <43001934+abhi291096@users.noreply.github.com>
2018-12-23 00:12:35 +0530

    Updated for Jenkins

--- BrowserStack.js
+++ BrowserStack.js
@@ -1,22 +1,27 @@
 var webdriver = require('browserstack-webdriver');
 
 var capabilities = {
-	'browserName' : 'firefox', 
-	'browserstack.user' : 'USERNAME',
-	'browserstack.key' : 'ACCESS_KEY'
+ 'browserName' : 'iPhone',
+ 'device' : 'iPhone 8 Plus',
+ 'realMobile' : 'true',
+ 'os_version' : '11.0',
+ 'browserstack.local': process.env.BROWSERSTACK_LOCAL || 'BROWSERSTACK_LOCAL'
+ // 'browserstack.user' : 'USERNAME',
+ // 'browserstack.key' : 'ACCESS_KEY'
+ 'browserstack.user': process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
+  'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY'
+  'browserstack.localIdentifier': process.env.BROWSERSTACK_LOCAL_IDENTIFIER || 'BROWSERSTACK_LOCAL_IDENTIFIER'
 }
 var driver = new webdriver.Builder().
-	usingServer('http://hub.browserstack.com/wd/hub').
-	withCapabilities(capabilities).
-	build();
+  usingServer('http://hub-cloud.browserstack.com/wd/hub').
+  withCapabilities(capabilities).
+  build();
 
-driver.get('http://www.google.com');
-driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack');
-driver.findElement(webdriver.By.name('btnG')).click();
-driver.wait(function() {
-	return driver.getTitle().then(function(title) {
-		return title === 'BrowserStack - Google Search';
-	});
-}, 1000);
-
-driver.quit();
+driver.get('http://www.google.com').then(function(){
+  driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack\n').then(function(){
+    driver.getTitle().then(function(title) {
+      console.log(title);
+      driver.quit();
+    });
+  });
+});
